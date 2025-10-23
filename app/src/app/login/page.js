aef3504de7df014bdad6bc25ca1fed2782279930
sah024 <sarah.tumelero.da.silveira@gmail.com>
2025-10-23 00:12:46 -0300

    front login
    Correção do HTML da página de login e estilização da página

--- app/src/app/login/page.js
+++ app/src/app/login/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import api from "@/lib/api";
+import "./login.css";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -18,28 +19,46 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <form
-        onSubmit={handleLogin}
-        className="w-96 p-6 rounded-2xl shadow bg-white space-y-4"
-      >
-        <h1 className="text-xl font-bold">Login</h1>
-        <input
-          type="email"
-          placeholder="E-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" className="w-full">
-          Entrar
-        </button>
-      </form>
+    <div className="login-background">
+      <div className="login-blob"></div>
+      <div className="login-container">
+        <h1 className="login-title">SIGMAA</h1>
+        <p className="login-subtitle">Sistema Integrado de Gerenciamento de Materiais e Atividades Acadêmicas</p>
+
+        <form onSubmit={handleLogin} className="login-form">
+          <div className="login-email">
+            <label htmlFor="email" className="login-label">Email</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Digite seu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="login-input"
+            />
+          </div>
+
+          <div className="login-password">
+            <label htmlFor="password" className="login-label">Senha</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Digite sua senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="login-input"
+            />
+            <p className="login-forgot-password">
+              Esqueceu sua senha? Sem problemas! 
+              <a href="#" className="forgot-password-link"> Clique aqui para iniciar a recuperação de senha</a>
+            </p>
+          </div>
+
+          <button type="submit" className="login-button">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
