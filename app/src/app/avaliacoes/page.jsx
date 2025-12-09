a1af1df807cdeea5836706a3f9baf534e5bc77ed
sah024 <sarah.tumelero.da.silveira@gmail.com>
2025-12-08 22:43:16 -0300

    css avaliacoes

--- app/src/app/avaliacoes/page.jsx
+++ app/src/app/avaliacoes/page.jsx
@@ -7,7 +7,9 @@ import { useApp } from "@/context/AppProvider";
 import Header from "@/components/Header/Header";
 import "./av.css";
 
+
 export default function Avaliacoes() {
+  const { user } = useApp();
   const [avaliacoes, setAvaliacoes] = useState([]);
   const [modalOpen, setModalOpen] = useState(false); 
   const [form, setForm] = useState({
@@ -102,70 +104,75 @@ export default function Avaliacoes() {
 };
 
   return (
-    <div className="page-container">
+    <div className="avaliacoes-page">
+      <Header name={user?.nome} />
+      <main className="avaliacao-main">
 
-      <button className="btn-add" onClick={() => setModalOpen(true)}>Nova Avaliação</button>
+          <button className="btn-add" onClick={() => setModalOpen(true)}>Nova Avaliação</button>
 
-      <div>
-        {avaliacoes.map((aval) => (
-          <div key={aval.id} className="avaliacao-card">
-            <h2>{aval.desc}</h2>
-            <p>Data: {aval.data}</p>
-            <p>Horário: {aval.horario}</p>
+          <div className="cards-container">
+            {avaliacoes.map((aval) => (
+              <div key={aval.id} className="avaliacao-card">
+                <div className="avaliacao-info">
+                  <h2>{aval.desc}</h2>
+                  <p>Data: {aval.data}</p>
+                  <p>Horário: {aval.horario}</p>
+                </div>
 
-          <div className="card-actions">
-            <button className="btn-delete" onClick={() => excluirAvaliacao(aval.id)}>Excluir</button>
-            <button className="btn-edit" onClick={() => {
-              setEditId(aval.id);
-              setForm({
-                desc: aval.desc,
-                data: aval.data,
-                horario: aval.horario,
-                disciplina_id: aval.disciplina_id,
-              });
-              setModalOpen(true);
-            }}
-            >Editar</button>
+              <div className="card-actions">
+                <button className="btn-delete" onClick={() => excluirAvaliacao(aval.id)}>Excluir</button>
+                <button className="btn-edit" onClick={() => {
+                  setEditId(aval.id);
+                  setForm({
+                    desc: aval.desc,
+                    data: aval.data,
+                    horario: aval.horario,
+                    disciplina_id: aval.disciplina_id,
+                  });
+                  setModalOpen(true);
+                }}
+                >Editar</button>
 
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {modalOpen && (
-        <div className="modal-overlay">
-          <div className="modal-box">
-
-            <h2>{editId ? "Editar Avaliação" : "Nova Avaliação"}</h2>
-
-            <form onSubmit={salvarAvaliacao}>
-
-              <input type="text" placeholder="Descrição" value={form.desc} onChange={(e) => setForm({ ...form, desc: e.target.value })} required/>
-
-              <input type="date" placeholder="Data" value={form.data} onChange={(e) => setForm({ ...form, data: e.target.value })} required/>
-
-              <input type="text" placeholder="Horário" value={form.horario} onChange={(e) => setForm({ ...form, horario: e.target.value })} required/>
-
-              <input type="hidden" value={form.disciplina_id} onChange={(e) => setForm({ ...form, disciplina_id: e.target.value })} required/>
-
-              {editId && (
-                  <input
-                    type="hidden"
-                    name="id"
-                    value={editId}
-                  />
-              )}
-
-              <div className="modal-actions">
-                <button type="button" className="btn-cancel" onClick={() => setModalOpen(false)}>Cancelar</button>
-
-                <button type="submit" className="btn-save">Salvar</button>
+                </div>
               </div>
-            </form>
-
+            ))}
           </div>
-        </div>
-      )}
+
+          {modalOpen && (
+            <div className="modal-overlay">
+              <div className="modal-box">
+
+                <h2>{editId ? "Editar Avaliação" : "Nova Avaliação"}</h2>
+
+                <form onSubmit={salvarAvaliacao}>
+
+                  <input type="text" placeholder="Descrição" value={form.desc} onChange={(e) => setForm({ ...form, desc: e.target.value })} required/>
+
+                  <input type="date" placeholder="Data" value={form.data} onChange={(e) => setForm({ ...form, data: e.target.value })} required/>
+
+                  <input type="text" placeholder="Horário" value={form.horario} onChange={(e) => setForm({ ...form, horario: e.target.value })} required/>
+
+                  <input type="hidden" value={form.disciplina_id} onChange={(e) => setForm({ ...form, disciplina_id: e.target.value })} required/>
+
+                  {editId && (
+                      <input
+                        type="hidden"
+                        name="id"
+                        value={editId}
+                      />
+                  )}
+
+                  <div className="modal-actions">
+                    <button type="button" className="btn-cancel" onClick={() => setModalOpen(false)}>Cancelar</button>
+
+                    <button type="submit" className="btn-save">Salvar</button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          )}
+      </main>
     </div>
   );
 }
